d11cb42e84dd1001d52f54c00c1e4f66965a3da0
Fabien <hos.fabien@outlook.com>
2024-09-21 21:22:26 +0200

    changement import des fonctions

--- initialize_db.py
+++ initialize_db.py
@@ -1,4 +1,4 @@
-from app.models import init_database, create_merged_data_table
+from database.db_operations import init_database, create_merged_data_table
 
 if __name__ == "__main__":
     init_database()
